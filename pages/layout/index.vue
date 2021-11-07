d76f30c4df04c7a62712e4100aed2217ef4aa7fc
chan.li <1123821016@qq.com>
2021-11-07 22:35:52 +0800

    发布部署-测试

--- pages/layout/index.vue
+++ pages/layout/index.vue
@@ -44,7 +44,7 @@
         <span class="attribution">
           An interactive learning project from
           <a href="https://thinkster.io">Thinkster</a>. Code &amp; design
-          licensed under MIT.
+          licensed under MIT
         </span>
       </div>
     </footer>
